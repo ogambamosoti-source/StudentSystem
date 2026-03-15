514b67db6cbde852fbc8a2b9e92628ceb57d087f
ogambamosoti-source <ogambamosoti@gmail.com>
2026-03-15 13:21:52 +0300

    Implement Course class with properties and methods

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,1 +1,45 @@
+package model;
+
+public class Course {
+private String courseName;
+private String courseCode;
+private int creditHours;
+private int departmentId;
+
+public Course(String courseName,String courseCode,int creditHours,int depertmentId){
+    this.courseName = courseName;
+    this.courseCode = courseCode;
+    this.creditHours = creditHours;
+    this.departmentId = depertmentId;
+}
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    } 
+    public int getDepartmentId(){
+        return departmentId;
+    }
+    public void setDepartmentId(int departmentId){
+        this.departmentId = departmentId;
+    }
+
+       @Override
+    public String toString() {
+        return "Course{" +
+                ", courseName='" + courseName + '\'' +
+                ", courseCode='" + courseCode + '\'' +
+                ", creditHours=" + creditHours +
+                ", departmentId= " + departmentId +
+                '}';
+    }
+
+}
 
