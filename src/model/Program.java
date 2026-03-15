49ea0e4db8c30b94f9180fcceeb94067ce082265
ogambamosoti-source <ogambamosoti@gmail.com>
2026-03-15 13:28:41 +0300

    Update Program.java

--- src/model/Program.java
+++ src/model/Program.java
@@ -1,1 +1,53 @@
+package model;
+public class Program {
+    private String programName;
+    private int programID;
+    private LevelEnum level; // e.g., DEGREE, DIPLOMA, etc.
+    private String durationYears;
+    private int departmentId;
+
+    public Program(String programName, int programID, LevelEnum level, String durationYears,int departmentId) {
+        this.programName = programName;
+        this.programID = programID;
+        this.level = level;
+        this.durationYears = durationYears;
+        this.departmentId = departmentId;
+    }
+
+    public Program(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public int getProgramID() {
+        return programID;
+    }
+
+    public LevelEnum getLevel() {
+        return level;
+    }
+
+    public String getDurationYears() {
+        return durationYears;
+    }
+    public int getDepartmentId(){
+        return departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Program{" +
+                "programName='" + programName + '\'' +
+                ", programID=" + programID +
+                ", level=" + level +
+                ", durationYears='" + durationYears + '\'' +
+                '}';
+    }
+}
+
+
+
 
