9f9a4b68879bd37d0849674f717be77e29fc061e
ogambamosoti-source <ogambamosoti@gmail.com>
2026-03-15 13:21:26 +0300

    Implement Book class with attributes and methods
    Added a Book class with attributes and methods.

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,1 +1,45 @@
+package model;
+
+
+public class Book{
+    private String title;
+    private String author;
+    private String isbn;
+    private String description;
+
+
+    public Book(String title,String author,String isbn,String description){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.description = description;
+    }
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Optional: toString for easy printing
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
 
