710f84fc34d972873684716555d335bfcb36595f
ogambamosoti-source <ogambamosoti@gmail.com>
2026-03-15 13:22:48 +0300

    Add Department class with name and description fields

--- src/model/Department.java
+++ src/model/Department.java
@@ -1,1 +1,30 @@
+package model;
+
+public class Department{
+    private String name;
+    private String description;
+
+    public Department(String name,String description){
+        this.name = name;
+        this.description = description;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String toString(){
+        return "Department{"+
+        "name='"+ name + '\''+
+        "descreption='"+ description + '\''+
+        '}';
+    }
+}
 
